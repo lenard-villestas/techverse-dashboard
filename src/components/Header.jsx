92760c49372bd9d2c0b9a4a90aa09366f900b934
lenard-villestas <lenard.villestas@outlook.com>
2023-04-01 01:57:22 -0600

    fixes time header bug
    fixes live clock to only render on mount to avoid client vs render mismatch

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,30 @@
 import React, { useState, useEffect } from 'react';
 
-const Header= () => {
-    const [time, setTime] = useState(new Date());
-    useEffect(() => {
-        // Update the time every second
-        const intervalId = setInterval(() => {
-          setTime(new Date());
-        }, 1000);
-        // Clear the interval when the component is unmounted
-    return () => clearInterval(intervalId);
-}, []);
+const Header = () => {
+  const [time, setTime] = useState(null);
 
-const localTime = time.toLocaleTimeString();
+
+  useEffect(() => {
+    
+    // Set the initial time after the component has been mounted
+    setTime(new Date());
+
+    // Update the time every second
+    const intervalId = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    // Clear the interval when the component is unmounted
+    return () => clearInterval(intervalId);
+  }, []);
+
+  /**This will ensure that the server-rendered HTML does not contain any time-related content, and the time will be displayed after the component has been mounted on the client-side. */
+  const localTime = time ? time.toLocaleTimeString() : '';
+
   return (
     <div className='Header'>
-        <h1 className='header'>Techverse Dashboard</h1>
-        <span className='time'>Calgary, AB (GMT-6) {localTime}</span>
+      <h1 className='header'>Techverse Dashboard</h1>
+      <span className='time'>Calgary, AB (GMT-6) {localTime}</span>
     </div>
   );
 }
