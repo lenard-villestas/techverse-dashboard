dfbcf5528f334e6675a02d6e716aab3b8fbb4194
lenard-villestas <lenard.villestas@outlook.com>
2023-04-01 01:20:27 -0600

    added live time on header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
-export default function 
-() {
+const Header= () => {
+    const [time, setTime] = useState(new Date());
+    useEffect(() => {
+        // Update the time every second
+        const intervalId = setInterval(() => {
+          setTime(new Date());
+        }, 1000);
+        // Clear the interval when the component is unmounted
+    return () => clearInterval(intervalId);
+}, []);
+
+const localTime = time.toLocaleTimeString();
   return (
     <div className='Header'>
         <h1 className='header'>Techverse Dashboard</h1>
+        <span className='time'>Calgary, AB (GMT-6) {localTime}</span>
     </div>
-  )
+  );
 }
+
+export default Header;
